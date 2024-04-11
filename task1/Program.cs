5574b650c18b932d1019e3c8a23326a4d31f73ed
tolmacheva <tolmacheva.july920506@gmail.com>
2024-04-11 10:08:42 +0300

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 1: Напишите программу, которая принимает 
+на вход число и проверяет, кратно ли оно 
+одновременно 7 и 23 */
+
+void MultipleOfNumber (int num)
+{
+    int seven = num / 7;
+    int twentyThree = num / 23;
+
+    if (seven * 7 == num && twentyThree * 23 == num)
+    {
+        System.Console.WriteLine($"{num} -> yes");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num} -> no");
+    }
+}
+
+System.Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+MultipleOfNumber (num);
+
