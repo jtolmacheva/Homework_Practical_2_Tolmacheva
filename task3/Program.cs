fe750ac0bd34f1c118dbcd08ad769e381a15b3ef
tolmacheva <tolmacheva.july920506@gmail.com>
2024-04-11 10:09:14 +0300

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 3: Напишите программу, которая принимает 
+на вход целое число из отрезка [10, 99] и показывает 
+наибольшую цифру числа */
+
+void FindMaxNumber(int num)
+{
+    if (num >= 10 && num <= 99)
+    {
+        int dec = num / 10;
+        int ed = num - (dec * 10);
+        
+
+        if (ed > dec)
+        {
+            System.Console.WriteLine($"{ed} -> max");
+        }
+
+        if (ed < dec) 
+        {
+            System.Console.WriteLine($"{dec} -> max");
+
+        }
+
+        if (ed == dec) 
+        {
+            System.Console.WriteLine("Numbers are equal");
+
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Input number from 10 to 99");
+    }
+
+}
+
+System.Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+FindMaxNumber(num);
